ac672f4ba869c9f72d8e2aff1470ee7f3606419d
Martin Flischman <wipes_meshes.03@icloud.com>
2023-06-30 16:52:16 +0200

    Initial Commit

---
+++ Unity/UnityApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnityApp.swift
+//  Unity
+//
+//  Created by Martin Flischman on 2023/06/30.
+//
+
+import SwiftUI
+
+@main
+struct UnityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
